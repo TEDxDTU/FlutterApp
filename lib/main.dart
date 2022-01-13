6750f1aefcd3364646e84c384072fac7518cde25
SatvikGupta <satvik.sli@gmail.com>
2022-01-13 23:11:29 +0530

    Added basic theming

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tedx_dtu_app/helpers/classes/ui_helper.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,22 +8,105 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    const Color primary = const Color(0xffE62B1E);
+
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.black,
+        pageTransitionsTheme: const PageTransitionsTheme(
+          builders: {
+            TargetPlatform.android: CupertinoPageTransitionsBuilder(),
+            TargetPlatform.iOS: CupertinoPageTransitionsBuilder(),
+          },
+        ),
+        snackBarTheme: SnackBarThemeData(
+          behavior: SnackBarBehavior.floating,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          backgroundColor: Colors.black87,
+        ),
+        primarySwatch: UIHelper.generateMaterialColor(const Color(0xFFE62B1E)),
+        primaryColor: const Color(0xFFE62B1E),
+        colorScheme: ColorScheme(
+          secondary: Colors.grey[800]!,
+          primary: primary,
+          background: Colors.black,
+          onBackground: Colors.white,
+          brightness: Brightness.dark,
+          error: Colors.red[800]!,
+          onError: Colors.white,
+          onPrimary: Colors.white,
+          onSecondary: Colors.white,
+          onSurface: Colors.white,
+          surface: Colors.red,
+          secondaryVariant: Colors.black,
+          primaryVariant: Colors.red[700]!,
+        ),
+        cardTheme: CardTheme(
+          color: const Color(0xffE62B1E),
+          elevation: 0,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(17),
+          ),
+        ),
+        outlinedButtonTheme: OutlinedButtonThemeData(
+          style: OutlinedButton.styleFrom(
+            backgroundColor: Colors.white,
+            shape: const StadiumBorder(),
+            primary: Colors.black,
+            side: const BorderSide(
+              color: Colors.black,
+              width: 1.3,
+            ),
+            onSurface: Colors.red,
+          ),
+        ),
+        textButtonTheme: TextButtonThemeData(
+          style: TextButton.styleFrom(
+            primary: Colors.white,
+            onSurface: Colors.red,
+          ),
+        ),
+        iconTheme: const IconThemeData(color: primary),
+        appBarTheme: AppBarTheme(
+          centerTitle: true,
+          backgroundColor: Colors.white12,
+          elevation: 0,
+          titleTextStyle: TextStyle(
+              color: primary, fontSize: 20, fontWeight: FontWeight.w700),
+          iconTheme: const IconThemeData(color: primary, size: 45),
+        ),
+        inputDecorationTheme: InputDecorationTheme(
+          floatingLabelBehavior: FloatingLabelBehavior.never,
+          // filled: true,
+          disabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            // borderSide: BorderSide.none,
+            borderSide: BorderSide(
+              color: Colors.grey,
+            ),
+          ),
+          // fillColor: const Color(0xFFffffff),
+
+          border: OutlineInputBorder(
+            gapPadding: 5,
+            borderRadius: BorderRadius.circular(10),
+          ),
+          enabledBorder: OutlineInputBorder(
+            gapPadding: 5,
+            borderRadius: BorderRadius.circular(10),
+            borderSide: BorderSide(
+              color: primary,
+            ),
+          ),
+          errorStyle:
+              const TextStyle(fontSize: 12, fontWeight: FontWeight.w500),
+          contentPadding: const EdgeInsets.all(0),
+        ),
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -31,15 +115,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -52,47 +127,18 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
@@ -102,6 +148,18 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.headline4,
             ),
+            Card(
+              child: SizedBox(
+                height: 200,
+                width: 200,
+              ),
+            ),
+            ElevatedButton(onPressed: () {}, child: Text('ElevatedButton')),
+            TextButton(onPressed: () {}, child: Text('TextButton')),
+            OutlinedButton(onPressed: () {}, child: Text('OutlinedButton')),
+            TextField(
+              enabled: false,
+            ),
           ],
         ),
       ),
@@ -109,7 +167,7 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
