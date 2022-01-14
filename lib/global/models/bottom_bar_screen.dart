7dd48e48f7aa8f01150365bff407ad843430017e
SatvikGupta <satvik.sli@gmail.com>
2022-01-15 04:04:35 +0530

    Document BottomBarScreen

--- lib/global/models/bottom_bar_screen.dart
+++ lib/global/models/bottom_bar_screen.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:tedx_dtu_app/global/widgets/bottom_bar_screen_widget.dart';
 
 class BottomBarScreen {
+  ///String title, displayed in nav bar when selected.
   final String title;
+
+  ///Icon, displayed in nav bar.
   final Icon icon;
+
+  ///Widget(Screen, should preferably be a [BottomBarScreenWidget]) that is
+  ///shown in the main area of the app when the item is selected.
   final Widget screen;
+
+  ///Creates a BottomBarScreen model. This is passed to TabsScreen to use in
+  ///bottom nav bar and display in the main area of the screen.
   BottomBarScreen({
     required this.title,
     required this.icon,
