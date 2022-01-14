43abd2f3e3587ffd10f1c8552e4c05be2e043617
SatvikGupta <satvik.sli@gmail.com>
2022-01-14 18:35:55 +0530

    Added reversing to loading spinner

--- lib/global/widgets/tedx_loading_spinner.dart
+++ lib/global/widgets/tedx_loading_spinner.dart
@@ -15,9 +15,15 @@ class _TedxLoadingSpinnerState extends State<TedxLoadingSpinner>
 
   double initialSpeed = 20;
   double deceleration = 10;
-
+  double totalTime = 3000;
   double angle(double time) {
-    double val = (initialSpeed * time - deceleration * time * time);
+    double val;
+    int mult = 1;
+    if (_animationController.status != AnimationStatus.forward) {
+      time = 1 - time;
+      mult = -1;
+    }
+    val = (initialSpeed * time - deceleration * time * time) * mult;
     return val - val.truncate();
   }
 
@@ -25,12 +31,12 @@ class _TedxLoadingSpinnerState extends State<TedxLoadingSpinner>
   void initState() {
     _animationController = AnimationController(
       vsync: this,
-      duration: const Duration(milliseconds: 3000),
+      duration: Duration(milliseconds: totalTime.toInt()),
     );
-    // _animationController.repeat(reverse: true);
-    // _animationController.addListener(() {
-    //   if (_animationController.status==AnimationStatus.)
-    // });
+    _animationController.repeat(
+      reverse: true,
+    );
+
     super.initState();
   }
 
