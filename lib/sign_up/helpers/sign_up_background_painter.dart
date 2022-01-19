9864fdd206927062c87874578f079a8737726cb8
SatvikGupta <satvik.sli@gmail.com>
2022-01-20 00:16:16 +0530

    Sign up Screen curvy background

---
+++ lib/sign_up/helpers/sign_up_background_painter.dart
@@ -0,0 +1,151 @@
+import 'package:flutter/material.dart';
+
+class SignUpBackgroundPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    var paint = Paint();
+    paint.color = Colors.white;
+    paint.style = PaintingStyle.stroke;
+    var width = size.width;
+    var height = size.height;
+    paint.strokeWidth = 3;
+    // var path = Path();
+    var path = Path()
+      ..moveTo(0, 0)
+      ..quadraticBezierTo(
+        width * 0.225,
+        height * 0.43,
+        width * 0.4,
+        height * 0.3,
+      )
+      ..quadraticBezierTo(
+        width * 0.6,
+        height * 0.1,
+        width * 0.76,
+        height * 0.3,
+      )
+      ..quadraticBezierTo(
+        width * 0.9,
+        height * 0.5,
+        width,
+        height * 0.5,
+      );
+    canvas.drawPath(path, paint);
+    path = Path();
+    paint.color = Colors.red;
+    path.moveTo(width, height * 0.3);
+    path
+      ..quadraticBezierTo(
+        width * 0.8,
+        height * 0.3,
+        width * 0.7,
+        height * 0.5,
+      )
+      ..quadraticBezierTo(
+        width * 0.6,
+        height * 0.7,
+        width * 0.4,
+        height * 0.5,
+      )
+      ..quadraticBezierTo(
+        width * 0.2,
+        height * 0.3,
+        0,
+        height * 0.3,
+      );
+    canvas.drawPath(path, paint);
+
+    path = Path();
+    paint.color = Colors.white;
+    path
+      ..moveTo(0, height * 0.5)
+      ..quadraticBezierTo(
+        width * 0.2,
+        height * 0.4,
+        width * 0.3,
+        height * 0.7,
+      )
+      ..quadraticBezierTo(
+        width * 0.4,
+        height * 0.9,
+        width * 0.5,
+        height * 0.7,
+      )
+      ..quadraticBezierTo(
+        width * 0.7,
+        height * 0.4,
+        width * 0.8,
+        height * 0.7,
+      )
+      ..quadraticBezierTo(
+        width * 0.9,
+        height * 0.9,
+        width,
+        height * 0.6,
+      );
+
+    ;
+
+    // canvas.drawPath(path, paint);
+    // paint.color = Colors.white70;
+    // path = Path()
+    //   ..moveTo(0, 0)
+    //   ..quadraticBezierTo(
+    //     width * 0.375,
+    //     height * 0.83,
+    //     width,
+    //     height,
+    //   )
+    //   ..lineTo(width, 0)
+    //   ..close();
+    //   ..moveTo(width * 0.45, 0)
+    //   ..quadraticBezierTo(width * 0.4, 0, width * 0.22, height * 0.15)
+    //   ..quadraticBezierTo(0, height * 0.4, width * 0.51, height * 0.3)
+    //   ..quadraticBezierTo(
+    //     width * 0.85,
+    //     height * 0.2,
+    //     width * 0.88,
+    //     height * 0.5,
+    //   )
+    //   ..quadraticBezierTo(
+    //     width * 0.48,
+    //     height * 0.65,
+    //     width * 0.54,
+    //     height * 0.67,
+    //   )
+    //   ..lineTo(width, height)
+    //   ..lineTo(width, 0);
+    // canvas.drawPath(path, paint);
+    // paint.style = PaintingStyle.stroke;
+    // paint.strokeWidth = 3;
+    // var path = Path();
+    // var width = size.width;
+    // var height = size.height;
+    // path.moveTo(0, height);
+    // path.lineTo(width, height * 0.7);
+    // path.moveTo(0, height * 0.55);
+    // path.lineTo(width, height * 0.65);
+    // path.moveTo(0, height * 0.27);
+    // path.lineTo(width, height * 0.4);
+    // path.moveTo(0, height * 0.27);
+    // path.lineTo(width, height * 0.8);
+
+    // // path.lineTo(0, height * 0.55);
+    // path.moveTo(width, height * 0.27);
+    // path.lineTo(0, height * 0.7);
+    // path.moveTo(width, height * 0.27);
+    // path.lineTo(0, height * 0.1);
+    // path.moveTo(width * 0.2, height);
+    // path.lineTo(width, height * 0.9);
+    // path.moveTo(0, height * 0.7);
+    // path.lineTo(width, height);
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(SignUpBackgroundPainter oldDelegate) => false;
+
+  @override
+  bool shouldRebuildSemantics(SignUpBackgroundPainter oldDelegate) => false;
+}
--- lib/sign_up/screens/sign_up_screen.dart
+++ lib/sign_up/screens/sign_up_screen.dart
@@ -1,5 +1,8 @@
+import 'dart:math' as math;
+
 import 'package:flutter/material.dart';
 import 'package:tedx_dtu_app/global/widgets/tedx_app_bar.dart';
+import 'package:tedx_dtu_app/sign_up/helpers/sign_up_background_painter.dart';
 import 'package:tedx_dtu_app/sign_up/helpers/sign_up_image_outline_painter.dart';
 import 'package:tedx_dtu_app/sign_up/helpers/sign_up_image_clipper.dart';
 import 'package:tedx_dtu_app/sign_up/widgets/sign_up_image_widget.dart';
@@ -44,224 +47,237 @@ class _SignUpScreenState extends State<SignUpScreen> {
       body: Form(
         key: _formKey,
         child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+          child: Stack(
             children: [
-              const SignUpImageWidget(),
-              const SizedBox(
-                height: 10,
-              ),
-              Padding(
-                padding: EdgeInsets.only(
-                    left: 10.0, right: MediaQuery.of(context).size.width * 0.2),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Name',
-                      style: TextStyle(
-                        fontSize: 17,
-                        color: Colors.white,
-                        fontWeight: FontWeight.w300,
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    TextFormField(
-                      style: TextStyle(color: Colors.white),
-                      keyboardType: TextInputType.text,
-                      decoration:
-                          buildInputDecoration(Icons.person, 'Full Name'),
-                      validator: (String? value) {
-                        if (value!.isEmpty) {
-                          return 'Please Enter Name';
-                        }
-                        return null;
-                      },
-                      onSaved: (String? value) {
-                        name = value;
-                      },
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    Text(
-                      'Mobile',
-                      style: TextStyle(
-                          fontSize: 17,
-                          color: Colors.white,
-                          fontWeight: FontWeight.w300),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    TextFormField(
-                      style: TextStyle(color: Colors.white),
-                      keyboardType: TextInputType.name,
-                      decoration:
-                          buildInputDecoration(Icons.phone, "Phone Number"),
-                      validator: (String? value) {
-                        if (value!.isEmpty) {
-                          return 'Please enter phone no ';
-                        } else if (value.length < 9) {
-                          return 'Please enter valid phone Number';
-                        }
-                        return null;
-                      },
-                      onSaved: (String? value) {
-                        phone = value;
-                      },
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    Text(
-                      'University',
-                      style: TextStyle(
-                        fontSize: 17,
-                        color: Colors.white,
-                        fontWeight: FontWeight.w300,
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    TextFormField(
-                      style: TextStyle(color: Colors.white),
-                      keyboardType: TextInputType.text,
-                      decoration:
-                          buildInputDecoration(Icons.business, "University"),
-                      validator: (String? value) {
-                        if (value!.isEmpty) {
-                          return 'Please Enter Your University Name';
-                        }
-                        return null;
-                      },
-                      onSaved: (String? value) {
-                        name = value;
-                      },
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    Text(
-                      'Email',
-                      style: TextStyle(
-                        fontSize: 17,
-                        color: Colors.white,
-                        fontWeight: FontWeight.w300,
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    TextFormField(
-                      style: TextStyle(color: Colors.white),
-                      keyboardType: TextInputType.name,
-                      decoration: buildInputDecoration(Icons.email, "Email"),
-                      validator: (String? value) {
-                        if (value!.isEmpty) {
-                          return 'Please Enter Email';
-                        }
-                        if (!RegExp("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+.[a-z]")
-                            .hasMatch(value)) {
-                          return 'Please a valid Email';
-                        }
-                        return null;
-                      },
-                      onSaved: (String? value) {
-                        email = value;
-                      },
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    Text(
-                      'Password',
-                      style: TextStyle(
-                        fontSize: 17,
-                        color: Colors.white,
-                        fontWeight: FontWeight.w300,
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    TextFormField(
-                      style: TextStyle(color: Colors.white),
-                      keyboardType: TextInputType.name,
-                      decoration: buildInputDecoration(Icons.lock, "Password"),
-                      validator: (String? value) {
-                        if (value!.isEmpty) {
-                          return 'Please Enter Password';
-                        } else if (value.length < 8) {
-                          return 'Password size is weak';
-                        }
-                        return null;
-                      },
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    Text(
-                      'Confirm Password',
-                      style: TextStyle(
-                        fontSize: 17,
-                        color: Colors.white,
-                        fontWeight: FontWeight.w300,
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    TextFormField(
-                      style: TextStyle(color: Colors.white),
-                      keyboardType: TextInputType.name,
-                      decoration:
-                          buildInputDecoration(Icons.lock, "Confirm Password"),
-                      validator: (String? value) {
-                        if (value!.isEmpty) {
-                          return 'Please re-enter password';
-                        }
-                        if (password.text != confirmpassword.text) {
-                          return "Password does not match";
-                        }
-                        return null;
-                      },
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    Align(
-                      alignment: Alignment.centerRight,
-                      child: ElevatedButton(
-                        onPressed: () {
-                          if (_formKey.currentState!.validate()) {
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              SnackBar(
-                                content: Text('Processing Data'),
-                              ),
-                            );
-                          }
-                        },
-                        style: ElevatedButton.styleFrom(
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(17),
-                            ),
-                            onPrimary: Colors.white,
-                            primary: Colors.red,
-                            elevation: 4),
-                        child: const Text(
-                          'Sign Up',
-                          style: TextStyle(
-                              fontSize: 16, fontWeight: FontWeight.bold),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(height: 117),
-                  ],
+              Positioned.fill(
+                child: CustomPaint(
+                  painter: SignUpBackgroundPainter(),
                 ),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SignUpImageWidget(),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(
+                        left: 10.0,
+                        right: MediaQuery.of(context).size.width * 0.2),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Name',
+                          style: TextStyle(
+                            fontSize: 17,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w300,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        TextFormField(
+                          style: TextStyle(color: Colors.white),
+                          keyboardType: TextInputType.text,
+                          decoration:
+                              buildInputDecoration(Icons.person, 'Full Name'),
+                          validator: (String? value) {
+                            if (value!.isEmpty) {
+                              return 'Please Enter Name';
+                            }
+                            return null;
+                          },
+                          onSaved: (String? value) {
+                            name = value;
+                          },
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          'Mobile',
+                          style: TextStyle(
+                              fontSize: 17,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w300),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        TextFormField(
+                          style: TextStyle(color: Colors.white),
+                          keyboardType: TextInputType.name,
+                          decoration:
+                              buildInputDecoration(Icons.phone, "Phone Number"),
+                          validator: (String? value) {
+                            if (value!.isEmpty) {
+                              return 'Please enter phone no ';
+                            } else if (value.length < 9) {
+                              return 'Please enter valid phone Number';
+                            }
+                            return null;
+                          },
+                          onSaved: (String? value) {
+                            phone = value;
+                          },
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          'University',
+                          style: TextStyle(
+                            fontSize: 17,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w300,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        TextFormField(
+                          style: TextStyle(color: Colors.white),
+                          keyboardType: TextInputType.text,
+                          decoration: buildInputDecoration(
+                              Icons.business, "University"),
+                          validator: (String? value) {
+                            if (value!.isEmpty) {
+                              return 'Please Enter Your University Name';
+                            }
+                            return null;
+                          },
+                          onSaved: (String? value) {
+                            name = value;
+                          },
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          'Email',
+                          style: TextStyle(
+                            fontSize: 17,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w300,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        TextFormField(
+                          style: TextStyle(color: Colors.white),
+                          keyboardType: TextInputType.name,
+                          decoration:
+                              buildInputDecoration(Icons.email, "Email"),
+                          validator: (String? value) {
+                            if (value!.isEmpty) {
+                              return 'Please Enter Email';
+                            }
+                            if (!RegExp(
+                                    "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+.[a-z]")
+                                .hasMatch(value)) {
+                              return 'Please a valid Email';
+                            }
+                            return null;
+                          },
+                          onSaved: (String? value) {
+                            email = value;
+                          },
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          'Password',
+                          style: TextStyle(
+                            fontSize: 17,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w300,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        TextFormField(
+                          style: TextStyle(color: Colors.white),
+                          keyboardType: TextInputType.name,
+                          decoration:
+                              buildInputDecoration(Icons.lock, "Password"),
+                          validator: (String? value) {
+                            if (value!.isEmpty) {
+                              return 'Please Enter Password';
+                            } else if (value.length < 8) {
+                              return 'Password size is weak';
+                            }
+                            return null;
+                          },
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          'Confirm Password',
+                          style: TextStyle(
+                            fontSize: 17,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w300,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        TextFormField(
+                          style: TextStyle(color: Colors.white),
+                          keyboardType: TextInputType.name,
+                          decoration: buildInputDecoration(
+                              Icons.lock, "Confirm Password"),
+                          validator: (String? value) {
+                            if (value!.isEmpty) {
+                              return 'Please re-enter password';
+                            }
+                            if (password.text != confirmpassword.text) {
+                              return "Password does not match";
+                            }
+                            return null;
+                          },
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Align(
+                          alignment: Alignment.centerRight,
+                          child: ElevatedButton(
+                            onPressed: () {
+                              if (_formKey.currentState!.validate()) {
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  SnackBar(
+                                    content: Text('Processing Data'),
+                                  ),
+                                );
+                              }
+                            },
+                            style: ElevatedButton.styleFrom(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(17),
+                                ),
+                                onPrimary: Colors.white,
+                                primary: Colors.red,
+                                elevation: 4),
+                            child: const Text(
+                              'Sign Up',
+                              style: TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 117),
+                      ],
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
