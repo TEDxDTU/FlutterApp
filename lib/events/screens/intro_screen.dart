d893691d8ea33842e1961de010f013e013067d4c
GargNaman714 <garg.naman714@gmail.com>
2022-01-30 01:29:51 +0530

    added intro screen

---
+++ lib/events/screens/intro_screen.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_svg/svg.dart';
+
+class IntroScreen extends StatelessWidget {
+  const IntroScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var mediaQuery = MediaQuery.of(context);
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Stack(
+        alignment: Alignment.center,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.15,
+              ),
+              Container(
+                alignment: Alignment.center,
+                height: MediaQuery.of(context).size.height / 5,
+                width: MediaQuery.of(context).size.width / 1.3,
+                child: Image.asset('assets/images/logoWhite.png'),
+              ),
+              FittedBox(
+                fit: BoxFit.contain,
+                alignment: Alignment.center,
+                child: Padding(
+                  padding: EdgeInsets.symmetric(
+                      horizontal: mediaQuery.size.height * 0.05),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'x',
+                        style: TextStyle(color: Colors.red, fontSize: 24),
+                      ),
+                      Text(
+                        '=independently organized TED event',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height / 10,
+              ),
+            ],
+          ),
+          Positioned(
+            bottom: 0,
+            left: -25,
+            child: SvgPicture.asset(
+              "assets/images/cup_guy_2.svg",
+              width: MediaQuery.of(context).size.width * 0.85,
+            ),
+          ),
+          Positioned(
+            bottom: mediaQuery.size.height * 0.2,
+            left: mediaQuery.size.height * 0.06,
+            child: SizedBox(
+              height: mediaQuery.size.height * 0.065,
+              width: mediaQuery.size.width * 0.62,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Text(
+                  "Let's Start",
+                  style: TextStyle(color: Colors.white, fontSize: 16),
+                ),
+                style: ElevatedButton.styleFrom(
+                  shape: StadiumBorder(),
+                  primary: Colors.red,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/global/screens/test_screen.dart
+++ lib/global/screens/test_screen.dart
@@ -1,62 +1,119 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:tedx_dtu_app/events/widgets/single_event_widget.dart';
 import 'package:tedx_dtu_app/global/widgets/tedx_loading_spinner.dart';
 import 'package:intl/intl.dart';
+import 'package:flutter_svg_provider/flutter_svg_provider.dart' as svg_provider;
 
-class TestScreen extends StatefulWidget {
+class TestScreen extends StatelessWidget {
   const TestScreen({Key? key}) : super(key: key);
   static const routeName = '/test';
 
-  @override
-  State<TestScreen> createState() => _TestScreenState();
-}
-
-class _TestScreenState extends State<TestScreen> {
-  bool showSpinner = true;
-  String? title;
-  late DateTime eventTime;
-
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  void didChangeDependencies() {
-    title = ModalRoute.of(context)?.settings.arguments as String?;
-    super.didChangeDependencies();
-  }
-
-  var tedxLoadingSpinnerKey = GlobalKey<TedxLoadingSpinnerState>();
-
-  // String time='?';
-  // String time=DateFormat('kk:mm,dd-mm-yyyy').format(eventTime);
+  // bool showSpinner = true;
+  // String? title;
+  // late DateTime eventTime;
+  // var tedxLoadingSpinnerKey = GlobalKey<TedxLoadingSpinnerState>();
 
   @override
   Widget build(BuildContext context) {
+    var mediaQuery = MediaQuery.of(context);
     return Scaffold(
-      body: ListView(
+      backgroundColor: Colors.black,
+      body: Stack(
+        alignment: Alignment.center,
         children: [
-          SingleEventWidget(
-            eventDate: DateTime.now(),
-            eventDescription:
-                'Description\nDescription asdfkjahksdfhj kajh\n sdfk jashdkfjhaksfkjashdkfjhsakdfjhkajsdfhkajshdfkjashdfkjhaskdfjhsakdfjhksajhfkjdjsfhk jsa',
-            // eventDescription: 'hello',
-            eventId: '1',
-            eventName: 'Event Name',
-            eventVenue: 'Venue',
-            imageUrl: 'https://picsum.photos/id/1/200/300',
-            isUpcoming: false,
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.15,
+              ),
+              Container(
+                alignment: Alignment.center,
+                height: MediaQuery.of(context).size.height / 5,
+                width: MediaQuery.of(context).size.width / 1.3,
+                // padding: EdgeInsets.all(14),
+                child: Image.asset('assets/images/logoWhite.png'),
+              ),
+              FittedBox(
+                fit: BoxFit.contain,
+                alignment: Alignment.center,
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal:mediaQuery.size.height*0.05),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'x',
+                        style: TextStyle(color: Colors.red, fontSize: 24),
+                      ),
+                      Text(
+                        '=independently organized TED event',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height / 10,
+              ),
+            ],
           ),
-          SingleEventWidget(
-            eventDate: DateTime.now(),
-            eventDescription: 'Description\nDescription',
-            eventId: '1',
-            eventName: 'Event 2',
-            eventVenue: 'Venue2',
-            imageUrl: 'https://picsum.photos/id/1/200/300',
-            isUpcoming: true,
-            ticketPrice: 100,
+          // Positioned(
+          //   bottom: 0,
+          //   child: Image(
+          //     image: svg_provider.Svg(
+          //       "assets/cup_guy_2.svg",
+          //     ),
+          //     // width: MediaQuery.of(context).size.width,
+          //     width: 700,
+          //     height: 700 + 350,
+          //     color: Colors.white,
+          //     fit: BoxFit.cover,
+          //   ),
+          // ),
+          Positioned(
+            bottom: 0,
+            left: -25,
+            child: SvgPicture.asset(
+              "assets/images/cup_guy_2.svg",
+              // color: Colors.white,
+              // fit: BoxFit.fitWidth,
+              width: MediaQuery.of(context).size.width * 0.85,
+              // height: MediaQuery.of(context).size.height * 0.6,
+              // width: 400,
+              // height: 600,
+              //  fit: BoxFit.contain,
+              //  height: MediaQuery.of(context).size.height,
+              // height: 600,
+            ),
+          ),
+          Positioned(
+            bottom: mediaQuery.size.height * 0.2,
+            // left: 45,
+            left: mediaQuery.size.height * 0.06,
+            child: SizedBox(
+              height: mediaQuery.size.height * 0.065,
+              width: mediaQuery.size.width * 0.62,
+              // height: 50,
+              // width:230,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Text(
+                  "Let's Start",
+                  style: TextStyle(color: Colors.white, fontSize: 16),
+                ),
+                style: ElevatedButton.styleFrom(
+                  shape: StadiumBorder(),
+                  primary: Colors.red,
+                ),
+              ),
+            ),
           ),
         ],
       ),
@@ -86,3 +143,19 @@ appBar: AppBar(title: Text(title ?? 'TEST')),
             }),
       ),
       */
+
+/*Expanded(
+                child: Container(
+                  width: double.infinity,
+                  child: SvgPicture.asset(
+                    "assets/images/cup guy 2.svg",
+                    color: Colors.white,
+                    fit: BoxFit.fitWidth,
+
+                    //  fit: BoxFit.contain,
+                    //  height: MediaQuery.of(context).size.height,
+                    // height: 600,
+                  ),
+                ),
+              ),
+*/
