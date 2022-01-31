0682d604855a83da3d6751fe499a8010f62ada52
SatvikGupta <satvik.sli@gmail.com>
2022-02-01 01:55:09 +0530

    Fix EventInfo bugs

--- lib/events/widgets/event_info.dart
+++ lib/events/widgets/event_info.dart
@@ -2,108 +2,120 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 class EventInfo extends StatelessWidget {
-  EventInfo(
-      {required this.eventVenue,
-      required this.dateTime,
-      required this.eventDescription,
-      required this.height,required this.paddingVal});
+  const EventInfo({
+    Key? key,
+    required this.eventVenue,
+    required this.dateTime,
+    required this.eventDescription,
+    required this.marginVal,
+  }) : super(key: key);
   final String eventVenue;
   final DateTime dateTime;
   final String eventDescription;
-  final double height;
-  final double paddingVal;
+  final double marginVal;
 
-  Widget createListTile(IconData icon, String txt) {
-    return ListTile(
-      tileColor: Colors.grey[850],
-      leading: Icon(
-        icon,
-        color: Colors.white,
-      ),
-      title: Text(
-        txt,
-        style: TextStyle(color: Colors.white),
+  Widget _createListTile(IconData icon, String txt) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10),
+      child: Row(
+        children: [
+          Icon(
+            icon,
+            color: Colors.white,
+            // size: 30,
+          ),
+          SizedBox(
+            width: 25,
+          ),
+          Text(
+            txt,
+            style: TextStyle(
+              color: Colors.white,
+              // fontSize: 17,
+            ),
+          ),
+        ],
       ),
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    var mediaQuery = MediaQuery.of(context);
     return Container(
-      height: height,
-      padding: EdgeInsets.all(paddingVal),
-      child: Card(
+      // height: height,
+      padding: EdgeInsets.all(13),
+      margin: EdgeInsets.all(marginVal),
+      decoration: BoxDecoration(
         color: Colors.grey[850],
-        child: Padding(
-          padding: EdgeInsets.only(
-              left: mediaQuery.size.width * 0.05,
-              right: mediaQuery.size.width * 0.05,
-              top: mediaQuery.size.height * 0.15,
-              bottom: mediaQuery.size.height * 0.07),
-          child: Column(
-            children: [
-              createListTile(Icons.location_on, eventVenue),
-              createListTile(
-                Icons.calendar_today,
-                DateFormat('dd MMM,yyyy').format(
-                  DateTime.now(),
-                ),
-              ),
-              createListTile(
-                Icons.access_time_rounded,
-                DateFormat('hh:mm a').format(DateTime.now()),
-              ),
-              SizedBox(height: mediaQuery.size.height*0.025,),
-              Container(
-                decoration: BoxDecoration(
-                  color: Colors.grey[700],
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(12.0),
-                  child: Column(
-                    children: [
-                      Text(
-                        'ABOUT',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                        ),
-                      ),
-                      Text(
-                        eventDescription,
-                        style: TextStyle(color: Colors.white),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: mediaQuery.size.height * 0.12,
-              ),
-              SizedBox(
-                height: mediaQuery.size.height * 0.055,
-                width: mediaQuery.size.width * 0.9,
-                child: ElevatedButton(
-                  onPressed: () {},
-                  child: Text(
-                    'BOOK NOW',
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          _createListTile(Icons.location_on, eventVenue),
+          _createListTile(
+            Icons.calendar_today,
+            DateFormat('dd MMMM, yyyy').format(
+              DateTime.now(),
+            ),
+          ),
+          _createListTile(
+            Icons.access_time_rounded,
+            DateFormat('hh:mm a').format(DateTime.now()),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.grey[700],
+              borderRadius: BorderRadius.circular(12),
+            ),
+            width: double.infinity,
+            child: Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Column(
+                children: [
+                  Text(
+                    'About',
+                    textAlign: TextAlign.center,
                     style: TextStyle(
                       fontWeight: FontWeight.bold,
                       color: Colors.white,
+                      fontSize: 20,
                     ),
                   ),
-                  style: ElevatedButton.styleFrom(
-                    shape: StadiumBorder(),
-                    primary: Colors.red[600],
+                  SizedBox(
+                    height: 10,
                   ),
-                ),
+                  Text(
+                    eventDescription,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 15,
+                    ),
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
-        ),
+          SizedBox(height: 30),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text(
+              'BOOK NOW',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            style: ElevatedButton.styleFrom(
+              shape: StadiumBorder(),
+              primary: Colors.red[600],
+              minimumSize: Size.fromHeight(50),
+            ),
+          ),
+        ],
       ),
     );
   }
