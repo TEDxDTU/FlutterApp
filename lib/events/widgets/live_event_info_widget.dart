43b102b1fd56440ed151ec12e16771b7cc9a4b68
rohitmalik776 <rohitmalik776@outlook.com>
2022-03-01 01:10:08 +0530

    Improve UI of live event info

--- lib/events/widgets/live_event_info_widget.dart
+++ lib/events/widgets/live_event_info_widget.dart
@@ -1,5 +1,6 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
 
 import '../models/live_event_info.dart';
 
@@ -8,46 +9,79 @@ class LiveEventInfoWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    bool alignLeft = false;
+    bool alignLeft = true;
+    String previousText = '';
 
     return StreamBuilder(
       stream: LiveEventInfo.fetch(),
       builder: (context, snapshot) {
-        LiveEventInfo data = snapshot.data as LiveEventInfo;
-        return Column(
-          children: [
-            AutoSizeText(
-              data.text,
-              style: TextStyle(
-                color: Colors.white,
-              ),
-            ),
-            ...data.textImage.map((e) {
-              alignLeft = !alignLeft;
-              List<Widget> children = [
-                AutoSizeText(
-                  e.text,
-                  style: TextStyle(
-                    color: Colors.white,
-                  ),
+        if (snapshot.hasError) {
+          throw snapshot.error.toString();
+        }
+        if (snapshot.hasData) {
+          LiveEventInfo data = snapshot.data as LiveEventInfo;
+          return Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 16.0,
+                  vertical: 8,
                 ),
-                Expanded(child: Image.network(e.imageUrl)),
-              ];
-              if (alignLeft == true) {
-                var reversedChildren = children.reversed.toList();
-                children = reversedChildren;
-              }
-              return Row(
-                children: children,
-              );
-            }).toList(),
-          ],
-        );
+                child: AnimatedTextKit(
+                  repeatForever: true,
+                  animatedTexts: [
+                    ColorizeAnimatedText(
+                      data.text,
+                      colors: [
+                        Colors.white,
+                        Colors.white,
+                        const Color(0xffE62B1E),
+                        Colors.white,
+                      ],
+                      textStyle: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 28,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              ...data.textImage.map((e) {
+                alignLeft = !alignLeft;
+                List<Widget> children = [
+                  Expanded(
+                    flex: 3,
+                    child: AutoSizeText(
+                      e.text,
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 22,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 8),
+                  Expanded(flex: 8, child: Image.network(e.imageUrl)),
+                ];
+                if (alignLeft == true) {
+                  var reversedChildren = children.reversed.toList();
+                  children = reversedChildren;
+                }
+                return Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Row(
+                    children: children,
+                  ),
+                );
+              }).toList(),
+            ],
+          );
+        }
+        return Container();
       },
-      initialData: LiveEventInfo(
-        'Information about the event will appear here.',
-        [],
-      ),
+      // initialData: LiveEventInfo(
+      //   'Information about the event will appear here.',
+      //   [],
+      // ),
     );
   }
 }
