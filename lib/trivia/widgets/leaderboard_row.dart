945116bf8e050204a4b3623cd25329cfb15e0cd7
SatvikGupta <satvik.sli@gmail.com>
2022-03-20 18:53:32 +0530

    Finalise leaderboard UI

--- lib/trivia/widgets/leaderboard_row.dart
+++ lib/trivia/widgets/leaderboard_row.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:tedx_dtu_app/global/screens/future_screen_template.dart';
+import 'package:tedx_dtu_app/global/widgets/custom_image_widget.dart';
 import 'package:tedx_dtu_app/trivia/providers/leaderboard_provider.dart';
 
 class LeaderboardRow extends StatelessWidget {
@@ -21,26 +22,69 @@ class LeaderboardRow extends StatelessWidget {
           return Consumer<LeaderboardProvider>(
             builder: (context, leaderboardProvider, _) {
               return SizedBox(
-                height: 100,
+                height: 110,
                 child: ListView.builder(
                   scrollDirection: Axis.horizontal,
                   itemCount: leaderboardProvider.length,
+                  physics: BouncingScrollPhysics(),
+                  // primary: ,
                   itemBuilder: (context, index) {
-                    return Row(
-                      children: [
-                        CircleAvatar(
-                          backgroundImage: NetworkImage(
-                            leaderboardProvider.data[index].imageUrl,
+                    return Container(
+                      width: 115,
+                      padding: const EdgeInsets.only(right: 20.0),
+                      child: Column(
+                        children: [
+                          Stack(children: [
+                            CircleAvatar(
+                              backgroundColor: Theme.of(context).primaryColor,
+                              radius: 40,
+                              child: Padding(
+                                padding: const EdgeInsets.all(2.0),
+                                child: ClipOval(
+                                  child: CustomImageWidget(
+                                    url: leaderboardProvider
+                                        .data[index].imageUrl,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Positioned(
+                              top: 1,
+                              left: 1,
+                              child: Container(
+                                width: 22,
+                                height: 22,
+                                decoration: BoxDecoration(
+                                  border: Border.all(
+                                    width: 2,
+                                  ),
+                                  color: Theme.of(context).primaryColor,
+                                  // color: Colors.black,
+                                  borderRadius: BorderRadius.circular(50),
+                                ),
+                                child: Center(
+                                  child: Text(
+                                    (index + 1).toString(),
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 12,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ]),
+                          const SizedBox(height: 5),
+                          Text(
+                            leaderboardProvider.data[index].name,
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
                           ),
-                        ),
-                        Column(
-                          children: [
-                            Text(leaderboardProvider.data[index].name),
-                            Text(leaderboardProvider.data[index].points
-                                .toString()),
-                          ],
-                        ),
-                      ],
+                          // const SizedBox(height: 5),
+                          // Text(leaderboardProvider.data[index].points
+                          //     .toString()),
+                        ],
+                      ),
                     );
                   },
                 ),
