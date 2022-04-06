6b1210805df503934491b7e389b062f7392c9654
SatvikGupta <satvik.sli@gmail.com>
2022-04-06 14:32:02 +0530

    Make trivia widgets responsive

--- lib/global/screens/test_screen.dart
+++ lib/global/screens/test_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:tedx_dtu_app/events/screens/speaker_details_screen.dart';
 import 'package:tedx_dtu_app/events/widgets/ticket_widget.dart';
+import 'package:tedx_dtu_app/trivia/models/trivia.dart';
 import 'package:tedx_dtu_app/trivia/screens/trivia_screen.dart';
 
 import 'package:tedx_dtu_app/trivia/widgets/circular_spinner.dart';
@@ -18,6 +19,6 @@ class _TestScreenState extends State<TestScreen> {
   final _key = GlobalKey<RotatingWidgetState>();
   @override
   Widget build(BuildContext context) {
-    return SpeakerDetailsScreen();
+    return TriviaScreen();
   }
 }
--- lib/trivia/widgets/trivia_details_row_widget.dart
+++ lib/trivia/widgets/trivia_details_row_widget.dart
@@ -5,11 +5,12 @@ import 'labelled_icon.dart';
 class TriviaDetailsRowWidget extends StatelessWidget {
   TriviaDetailsRowWidget({
     Key? key,
+    Color? iconColor,
+    Color? dividerColor,
     required this.questionCount,
     required this.totalTime,
     this.textColor,
-    Color? iconColor,
-    Color? dividerColor,
+    this.width,
   })  : iconColor = iconColor ?? Colors.blueGrey[100]!,
         dividerColor = dividerColor ?? Colors.blueGrey[200]!,
         super(key: key);
@@ -19,39 +20,45 @@ class TriviaDetailsRowWidget extends StatelessWidget {
   final Color iconColor;
   final Color dividerColor;
   final Color? textColor;
+  final double? width;
   @override
   Widget build(BuildContext context) {
-    return Row(
-      // mainAxisSize: MainAxisSize.min,
-      mainAxisAlignment: MainAxisAlignment.end,
-      children: [
-        LabelledIcon(
-          label: '$questionCount Questions',
-          icon: Icon(
-            Icons.help,
-            color: iconColor,
-          ),
-          textColor: textColor,
+    return SizedBox(
+      width: width,
+      child: FittedBox(
+        child: Row(
+          // mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            LabelledIcon(
+              label: '$questionCount Questions',
+              icon: Icon(
+                Icons.help,
+                color: iconColor,
+              ),
+              textColor: textColor,
+            ),
+            VerticalDivider(thickness: 3, color: dividerColor),
+            LabelledIcon(
+              label: '$totalTime secs',
+              icon: Icon(
+                Icons.timer,
+                color: iconColor,
+              ),
+              textColor: textColor,
+            ),
+            VerticalDivider(thickness: 3, color: dividerColor),
+            LabelledIcon(
+              label: '${questionCount * 10} Points',
+              icon: Icon(
+                Icons.emoji_events,
+                color: iconColor,
+              ),
+              textColor: textColor,
+            ),
+          ],
         ),
-        VerticalDivider(thickness: 3, color: dividerColor),
-        LabelledIcon(
-          label: '$totalTime secs',
-          icon: Icon(
-            Icons.timer,
-            color: iconColor,
-          ),
-          textColor: textColor,
-        ),
-        VerticalDivider(thickness: 3, color: dividerColor),
-        LabelledIcon(
-          label: '${questionCount * 10} Points',
-          icon: Icon(
-            Icons.emoji_events,
-            color: iconColor,
-          ),
-          textColor: textColor,
-        ),
-      ],
+      ),
     );
   }
 }
