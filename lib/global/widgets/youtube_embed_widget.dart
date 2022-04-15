414152f2f3e3e3d780df157d9e74080d29692f9c
SatvikGupta <satvik.sli@gmail.com>
2022-04-15 20:50:38 +0530

    Experimenting with YoutubePlayerIFrame

--- lib/global/widgets/youtube_embed_widget.dart
+++ lib/global/widgets/youtube_embed_widget.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:youtube_player_flutter/youtube_player_flutter.dart';
+import 'package:tedx_dtu_app/global/widgets/tedx_loading_spinner.dart';
+// import 'package:youtube_player_flutter/youtube_player_flutter.dart';
+import 'package:youtube_player_iframe/youtube_player_iframe.dart';
 
 import '../providers/config.dart';
 
@@ -13,27 +15,77 @@ class YoutubeEmbedWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    String videoId = YoutubePlayer.convertUrlToId(url)!;
+    String videoId = YoutubePlayerController.convertUrlToId(url)!;
     YoutubePlayerController _controller = YoutubePlayerController(
-      initialVideoId: videoId,
-      flags: YoutubePlayerFlags(
-          // autoPlay: true,
-          // mute: true,
-          ),
+        initialVideoId: videoId,
+        params: YoutubePlayerParams(
+          // showControls: false,
+          showVideoAnnotations: false,
+          useHybridComposition: false,
+          showFullscreenButton: true,
+          autoPlay: false,
+          strictRelatedVideos: true,
+        )
+        // flags: YoutubePlayerFlags(
+        //     // autoPlay: true,
+        //     // mute: true,
+        //     ),
+        );
+    _controller.hidePauseOverlay();
+    _controller.hideTopMenu();
+    // return YoutubePlayerBuilder(
+    //     player: YoutubePlayer(
+    //       controller: _controller,
+    //     ),
+    //     onEnterFullScreen: () {
+    //       Provider.of<Config>(context, listen: false).hideBottomBar();
+    //     },
+    //     onExitFullScreen: () {
+    //       Provider.of<Config>(context, listen: false).showBottomBar();
+    //     },
+    //     builder: (context, player) {
+    //       return player;
+    //     });
+    Widget player = YoutubePlayerIFrame(
+      key: ValueKey('player$url'),
+      controller: _controller,
     );
+    // bool _isInit = true;
+    return Stack(
+      children: [
+        YoutubeValueBuilder(
+            controller: _controller,
+            builder: (context, value) {
+              // _controller.
 
-    return YoutubePlayerBuilder(
-        player: YoutubePlayer(
-          controller: _controller,
+              print(DateTime.now());
+              _controller.hidePauseOverlay();
+              _controller.hideTopMenu();
+              // _isInit = false;
+              // _controller.
+              print("value $value");
+              return Stack(
+                children: [
+                  player,
+                ],
+              );
+              // : TedxLoadingSpinner();
+            }),
+        Positioned(
+          bottom: 0,
+          left: 0,
+          right: 0,
+          child: Slider(
+            value: _controller.value.position.inSeconds.toDouble(),
+            activeColor: Theme.of(context).primaryColor,
+            inactiveColor: Colors.grey,
+            max: _controller.value.metaData.duration.inSeconds.toDouble(),
+            onChanged: (pos) {
+              _controller.seekTo(Duration(seconds: pos.toInt()));
+            },
+          ),
         ),
-        onEnterFullScreen: () {
-          Provider.of<Config>(context, listen: false).hideBottomBar();
-        },
-        onExitFullScreen: () {
-          Provider.of<Config>(context, listen: false).showBottomBar();
-        },
-        builder: (context, player) {
-          return player;
-        });
+      ],
+    );
   }
 }
--- lib/helpers/constants/constants.dart
+++ lib/helpers/constants/constants.dart
@@ -6,9 +6,9 @@
 // Satvik iPhone Hotspot:
 // String nodeServerBaseUrl = 'http://172.20.10.2:3000';
 //Satvik Home:
-// String nodeServerBaseUrl = 'http://192.168.1.37:3000';
+String nodeServerBaseUrl = 'http://192.168.1.37:3000';
 //Saarthak Shivang PG:
-String nodeServerBaseUrl = 'http://192.168.1.8:3000';
+// String nodeServerBaseUrl = 'http://192.168.1.8:3000';
 
 //Sagar's pg:
 // String nodeServerBaseUrl = 'http://103.73.95.6:3000';
